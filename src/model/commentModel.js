ff80cf563e7816badc817137629c69e03349f2e5
Nam <namph2102@gmail.com>
2023-06-17 10:56:04 +0700

    upload

--- src/model/commentModel.js
+++ src/model/commentModel.js
@@ -8,6 +8,7 @@ const commentSchema = new Schema(
     room: { type: Schema.Types.ObjectId, required: true, ref: "Room" },
     comment: { type: String, required: true },
     author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    recipient: { type: Schema.Types.ObjectId, require: true },
     type: { type: String, default: "text", require: true },
     isSee: { type: Boolean, default: false },
   },
--- src/socket/index.js
+++ src/socket/index.js
@@ -2,38 +2,97 @@ import CommentModel from "../model/commentModel.js";
 import UserModel from "../model/userModel.js";
 import InfomationController from "../controller/InfomationController.js";
 const listAccountOnline = [];
+// {[iduser]:[{room1:true},{room2:false}]}
+const listUserJoinRoom = [];
+
 class handleSocketCall {
   socket;
   constructor(socket) {
     this.socket = socket;
 
     // chatting with tow user
-    socket.on("tao-room", (idroom) => {
+    socket.on("tao-room", async (idroom) => {
       socket.currentroom = idroom;
       socket.join(idroom);
+
+      if (socket.userid) {
+        try {
+          await CommentModel.updateMany(
+            { room: idroom, recipient: socket.userid },
+            { isSee: true }
+          );
+        } catch {}
+        if (listUserJoinRoom[socket.userid]) {
+          listUserJoinRoom[socket.userid] = {
+            ...listUserJoinRoom[socket.userid],
+            [idroom]: true,
+          };
+        } else {
+          listUserJoinRoom[socket.userid] = { [idroom]: true };
+        }
+      }
+    });
+    // user leave room
+    socket.on("leaver-room-chat-current", (idroom) => {
+      console.log("rời khỏi phòng", idroom);
+      console.log(socket.userid);
+
+      try {
+        if (idroom) {
+          socket.leave(idroom);
+          const roomCuurent = listUserJoinRoom[socket.userid];
+          if (roomCuurent[idroom]) {
+            roomCuurent[idroom] = false;
+          }
+          console.log(roomCuurent);
+        }
+      } catch {}
     });
 
     socket.on("user-chat", (data) => {
-      console.log(socket.currentroom);
       if (!socket.currentroom) return;
-      CommentModel.create({
-        room: socket.currentroom,
-        comment: data.comment,
-        author: data.author._id,
-        isSee: data.isSee,
-      });
-      socket.broadcast.to(socket.currentroom).emit("server-chat", data);
+      try {
+        console.log(data);
+        console.log(
+          listUserJoinRoom[socket.userid],
+          listUserJoinRoom[data.idPerson]
+        );
+        let isSeeUserSend = false;
+
+        // xui xui undefile là toang
+        if (listUserJoinRoom[data.idPerson]) {
+          if (
+            listUserJoinRoom[data.idPerson][socket.currentroom] == true ||
+            listUserJoinRoom[data.idPerson][socket.currentroom] == false
+          ) {
+            isSeeUserSend = listUserJoinRoom[data.idPerson][socket.currentroom];
+          }
+        }
+        CommentModel.create({
+          room: socket.currentroom,
+          comment: data.comment,
+          author: data.author._id,
+          isSee: isSeeUserSend,
+          recipient: data.idPerson,
+        });
+        socket.broadcast.to(socket.currentroom).emit("server-chat", data);
+        socket.emit("user-chat-message", { ...data, isSee: isSeeUserSend });
+      } catch {}
     });
 
     socket.on("disconnect", () => {
       if (socket.userid) {
         this.handleUserOffline(socket.userid);
+        if (listUserJoinRoom[socket.userid]) {
+          delete listUserJoinRoom[socket.userid];
+        }
         socket.broadcast.emit(`friend-chattings-${socket.userid}`, false);
       }
       listAccountOnline.splice(listAccountOnline.indexOf(socket.userid), 1);
     });
     socket.on("client-acttaced-id", async (userid) => {
       socket.join(userid);
+
       socket.userid = userid;
       const account = await UserModel.findByIdAndUpdate(userid, {
         status: true,

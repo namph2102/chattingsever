9307bcf51b5ad822d3c1ca42ab57796deb743a02
Nam <namph2102@gmail.com>
2023-07-31 17:56:19 +0700

    update delete comment

--- src/socket/userchat.js
+++ src/socket/userchat.js
@@ -10,69 +10,76 @@ class UserChatSocket {
     socket.on(
       "client-send-chatting-change",
       ({ id, userId, type, typeChatting }) => {
-        const action = { userId, kind: type };
-
-        if (type == "delete") {
-          CommentModel.findByIdAndUpdate(id, {
-            action,
-          }).then((result) => {
-            if (typeChatting != "text") {
-              if (userId == result.author) {
-                CommentModel.findByIdAndDelete(id).then(async (result) => {
-                  if (typeChatting == "audio" || typeChatting == "document") {
-                    if (result.comment) {
-                      await GoogleDrive.deletefile(result.comment);
-                    }
-                  }
-                  if (
-                    result.type == "image" &&
-                    result.file &&
-                    result.file.length > 0
-                  ) {
-                    result.file.forEach(
-                      async ({ path }) =>
-                        path && (await GoogleDrive.deletefile(path))
-                    );
-                  }
-                });
-              }
-            }
-
-            io.in(socket.currentroom).emit(`server-send-chatting-change`, {
+        try {
+          const action = { userId, kind: type };
+          if (type == "delete") {
+            CommentModel.findByIdAndUpdate(id, {
               action,
-              idComment: id,
-            });
-          });
-        } else if (type == "edit") {
-          CommentModel.findById(id).then((result) => {
-            if (result.author == userId) {
-              CommentModel.findByIdAndUpdate(id, {
-                comment: typeChatting,
-                action,
-              }).then((result) => {});
+            }).then(async (result) => {
+              if (typeChatting != "text") {
+                if (userId == result.author) {
+                  CommentModel.findByIdAndDelete(id).then(async (result) => {
+                    if (typeChatting == "audio" || typeChatting == "document") {
+                      if (result.comment) {
+                        await GoogleDrive.deletefile(result.comment);
+                      }
+                    }
+                    if (
+                      result.type == "image" &&
+                      result.file &&
+                      result.file.length > 0
+                    ) {
+                      result.file.forEach(
+                        async ({ path }) =>
+                          path && (await GoogleDrive.deletefile(path))
+                      );
+                    }
+                  });
+                }
+              } else {
+                if (userId == result?.author) {
+                  await CommentModel.findByIdAndDelete(id).then(() => {});
+                }
+              }
+
               io.in(socket.currentroom).emit(`server-send-chatting-change`, {
-                action: {
-                  userId,
-                  kind: type,
-                  newComment: typeChatting,
-                },
+                action,
                 idComment: id,
               });
-            }
-          });
-        } else if (type == "ghim") {
-          CommentModel.findByIdAndUpdate(id, { action }).then((result) => {
-            if (result.action.kind == "ghim") {
-              action.kind = "";
-              CommentModel.findByIdAndUpdate(id, { action: { kind: "" } }).then(
-                () => {}
-              );
-            }
-            io.in(socket.currentroom).emit(`server-send-chatting-change`, {
-              action,
-              idComment: id,
             });
-          });
+          } else if (type == "edit") {
+            CommentModel.findById(id).then((result) => {
+              if (result.author == userId) {
+                CommentModel.findByIdAndUpdate(id, {
+                  comment: typeChatting,
+                  action,
+                }).then((result) => {});
+                io.in(socket.currentroom).emit(`server-send-chatting-change`, {
+                  action: {
+                    userId,
+                    kind: type,
+                    newComment: typeChatting,
+                  },
+                  idComment: id,
+                });
+              }
+            });
+          } else if (type == "ghim") {
+            CommentModel.findByIdAndUpdate(id, { action }).then((result) => {
+              if (result.action.kind == "ghim") {
+                action.kind = "";
+                CommentModel.findByIdAndUpdate(id, {
+                  action: { kind: "" },
+                }).then(() => {});
+              }
+              io.in(socket.currentroom).emit(`server-send-chatting-change`, {
+                action,
+                idComment: id,
+              });
+            });
+          }
+        } catch (error) {
+          console.log(error);
         }
       }
     );
